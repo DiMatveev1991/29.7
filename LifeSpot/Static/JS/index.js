122b2a4c58f48790f823c2d69382327e30de8891
DiMatveev1991 <98545906+DiMatveev1991@users.noreply.github.com>
2023-04-09 18:33:30 +0300

    29.7
    29.7

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,55 @@
+﻿
+let session =  new Map();
+
+
+function handleSession(){
+   
+    session.set("startDate", new Date().toLocaleString())
+  
+    session.set("userAgent", window.navigator.userAgent)
+}
+
+
+function checkAge(){
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+    
+    if(session.get("age") >= 18){
+        alert("Приветствуем на LifeSpot! " + '\n' +  "Текущее время: " + new Date().toLocaleString() );
+    }
+    else{
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. ВыL будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+
+
+
+let sessionLog = function () {
+    for (let result of session){
+        console.log(result)
+    }
+}
+
+
+
+function filterContent(){
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i <= elements.length; i++ ){
+        let videoText = elements[i].getElementsByTagName('h3')[0].innerText;
+
+        if(!videoText.toLowerCase().includes(inputParseFunction().toLowerCase())){
+            elements[i].style.display = 'none';
+        } else {
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
+
+// setTimeout(() =>
+//     alert("Нравится LifeSpot? " + '\n' +  "Подпишитесь на наш Instagram @lifespot999!" ),
+// 7000);
+
+
+
+
---
+++ LifeSpot/Static/JS/testing.js
@@ -0,0 +1,17 @@
+﻿
+let myMap = new Map();
+myMap.set("Германия", "Берлин")
+myMap.set("Швеция", "Стокгольм")
+myMap.set(1, "Москва")
+
+let newArray = [];
+
+for (let result of myMap){
+    newArray.push(result);
+}
+console.log(newArray);
+
+let newArrayOfStrings = [];
+
+Array.from(myMap, ([key,value]) => newArrayOfStrings.push(`${key} - ${value}`) );
+console.log(newArrayOfStrings);
